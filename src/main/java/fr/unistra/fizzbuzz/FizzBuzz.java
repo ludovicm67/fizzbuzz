8134677ba4cf8c7783c15582f2fb35c99ac40d8c
ludovicm67 <ludovicmuller1@gmail.com>
2018-03-22 15:20:25 +0100

    implemented FizzBuzz

--- src/main/java/fr/unistra/fizzbuzz/FizzBuzz.java
+++ src/main/java/fr/unistra/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,14 @@ package fr.unistra.fizzbuzz;
 public class FizzBuzz {
 
   public String convert(int number) {
-    throw new RuntimeException("Not implemented");
+    if (number % 3 == 0 && number % 5 == 0) {
+      return "FizzBuzz";
+    } else if (number % 3 == 0) {
+      return "Fizz";
+    } else if (number % 5 == 0) {
+      return "Buzz";
+    } else {
+      return Integer.toString(number);
+    }
   }
 }
--- src/test/java/fr/unistra/fizzbuzz/FizzBuzzJUnitTest.java
+++ src/test/java/fr/unistra/fizzbuzz/FizzBuzzJUnitTest.java
@@ -18,4 +18,38 @@ public class FizzBuzzJUnitTest {
     assertEquals("13",fizzbuzz.convert(13));
     assertEquals("14",fizzbuzz.convert(14));
   }
+
+  @Test
+  public void returnsNumberForInputDivisibleByThreeAndFive() {
+    assertEquals("FizzBuzz", fizzbuzz.convert(15));
+    assertEquals("FizzBuzz", fizzbuzz.convert(30));
+    assertEquals("FizzBuzz", fizzbuzz.convert(45));
+    assertEquals("FizzBuzz", fizzbuzz.convert(60));
+    assertEquals("FizzBuzz", fizzbuzz.convert(75));
+    assertEquals("FizzBuzz", fizzbuzz.convert(90));
+  }
+
+  @Test
+  public void returnsNumberForInputDivisibleByThreeButNotByFive() {
+    assertEquals("Fizz", fizzbuzz.convert(3));
+    assertEquals("Fizz", fizzbuzz.convert(6));
+    assertEquals("Fizz", fizzbuzz.convert(9));
+    assertEquals("Fizz", fizzbuzz.convert(12));
+    assertEquals("Fizz", fizzbuzz.convert(18));
+    assertEquals("Fizz", fizzbuzz.convert(21));
+    assertEquals("Fizz", fizzbuzz.convert(24));
+    assertEquals("Fizz", fizzbuzz.convert(27));
+    assertEquals("Fizz", fizzbuzz.convert(33));
+  }
+
+  @Test
+  public void returnsNumberForInputDivisibleByFiveButNotByThree() {
+    assertEquals("Buzz", fizzbuzz.convert(5));
+    assertEquals("Buzz", fizzbuzz.convert(10));
+    assertEquals("Buzz", fizzbuzz.convert(20));
+    assertEquals("Buzz", fizzbuzz.convert(25));
+    assertEquals("Buzz", fizzbuzz.convert(35));
+    assertEquals("Buzz", fizzbuzz.convert(40));
+    assertEquals("Buzz", fizzbuzz.convert(50));
+  }
 }
